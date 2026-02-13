b4c6fd84718c0db45b51fdd22d2eeb952f5a7a01
Dhanya Hegde <155796110+Dhanyahegde1@users.noreply.github.com>
2026-02-13 12:06:57 +0530

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+$conn=mysqli_connect("localhost","root","","swiss_collection");
+// if(!$conn){
+//     echo " not connected";
+// }
+// else{
+//     echo "successfully connected";
+// }
+?>
---
+++ header.php
@@ -0,0 +1,17 @@
+<header class="header">
+
+   <div class="flex">
+
+      <a href="#" class="logo">PETZEE</a>
+
+      <nav class="navbar">
+         <a href="admin.php">add products</a>
+         <a href="products.php">view products</a>
+      </nav>
+      
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </div>
+
+</header>
